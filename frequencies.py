317ce12d219bb8141a9f02fae5bd7d9354f0a7a4
istiyakc <istiyak.choudhury@kcl.ac.uk>
2022-11-13 19:25:41 +0000

    first try

--- frequencies.py
+++ frequencies.py
@@ -1,9 +1,19 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    frequencies = dict.fromkeys(items, 0)
-    for i in frequencies:
-        frequencies[i] = items.count(frequencies[i])
+    list = [str(item) for item in items]
+    frequencies = dict.fromkeys(items,0)
+
+    for i in list:
+
+        if list[i] in frequencies:
+
+            frequencies[i] += 1
+        else:
+            frequencies[i] = 1
+
+
     return frequencies
